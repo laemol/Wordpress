05c83e4fe737b3cec6077f2dc34fb027e6bf5987
Leon <leon@molit.nl>
2018-12-27 19:48:12 +0100

    Slider

--- wp-content/themes/grandtour-child/templates/template-tour-header.php
+++ wp-content/themes/grandtour-child/templates/template-tour-header.php
@@ -1,34 +1,18 @@
 <?php
-/**
-*	Get Current page object
-**/
-$page = get_page($post->ID);
 
 // Get current product
 $product = array_shift(apiGetRequest('products/' . $_GET['pid']));
 
-/**
-*	Get current page id
-**/
-
-if (!isset($current_page_id) && isset($page->ID)) {
-    $current_page_id = $page->ID;
-}
+$image_thumb = [$product['media'][0]['imageUrl']];
 
 //Get page header display setting
-$page_title = get_the_title();
 $page_menu_transparent = 0;
 
 //Get tour header option
 $tg_tour_single_header = kirki_get_option('tg_tour_single_header');
 
 if (isset($product['media'])) {
-    $pp_page_bg = '';
-
-    //Get page featured image
-    $image_id = get_post_thumbnail_id($current_page_id);
-    $image_thumb = [$product['media'][0]['imageUrl']];
-
+    
     if (isset($image_thumb[0]) && !empty($image_thumb[0])) {
         $pp_page_bg = $image_thumb[0];
         $page_menu_transparent = 1;
@@ -37,98 +21,44 @@ if (isset($product['media'])) {
     $grandtour_topbar = grandtour_get_topbar();
     $grandtour_screen_class = grandtour_get_screen_class();
 
-    //Get header featured content
-    $tour_header_type = get_post_meta(get_the_ID(), 'tour_header_type', true);
+?>
 
-    $video_url = '';
+<div id="page_caption" class="hasbg parralax">
+    <!-- Slideshow container -->
+<div class="slideshow-container">
 
-    if ($tour_header_type == 'Youtube Video' or $tour_header_type == 'Vimeo Video') {
-        //Add jarallax video script
-        wp_enqueue_script('jarallax-video', get_template_directory_uri() . '/js/jarallax-video.js', false, GRANDTOUR_THEMEVERSION, true);
+<!-- Full-width images with number and caption text -->
+<?php 
+foreach($product['media'] as $image){ ?>
+<div class="mySlides fade">
+  <img src="<?php echo esc_url($image['imageUrl']); ?>" style="width:100%">
+</div>
 
-        if ($tour_header_type == 'Youtube Video') {
-            $tour_header_youtube = get_post_meta(get_the_ID(), 'tour_header_youtube', true);
-            $video_url = 'https://www.youtube.com/watch?v=' . $tour_header_youtube;
-        } else {
-            $tour_header_vimeo = get_post_meta(get_the_ID(), 'tour_header_vimeo', true);
-            $video_url = 'https://vimeo.com/' . $tour_header_vimeo;
-        }
-    }
+<?php } ?>
 
-    $parallax_class = 'parallax';
+<!-- Next and previous buttons -->
+<a class="prevslide nav" onclick="plusSlides(-1)"><div class="ti-angle-left"></div></a>
+<a class="nextslide nav" onclick="plusSlides(1)"><div class="ti-angle-right"></div></a>
+</div>
+<br>
 
-    //Check if add parallax effect
-    $tg_page_header_bg_parallax = kirki_get_option('tg_page_header_bg_parallax');
+<!-- The dots/circles -->
+<div class="dots">
+<?php 
+$slide = 1;
+foreach($product['media'] as $image){ ?>
+<span class="dot" onclick="currentSlide($slide)"></span> 
+<?php 
+$slide++ ;
+} ?>
+</div>
 
-    if ($tour_header_type == 'Image' && empty($tg_page_header_bg_parallax)) {
-        $parallax_class = '';
-    } ?>
-<div id="page_caption" class="<?php if (!empty($pp_page_bg)) {
-        ?>hasbg <?php echo esc_attr($parallax_class); ?><?php
-    } ?> <?php if (!empty($grandtour_topbar)) {
-        ?>withtopbar<?php
-    } ?> <?php if (!empty($grandtour_screen_class)) {
-        ?>split<?php
-    } ?>" <?php if (!empty($pp_page_bg)) {
-        ?>style="background-image:url(<?php echo esc_url($pp_page_bg); ?>);"<?php
-    } ?> <?php if ($tour_header_type == 'Youtube Video' or $tour_header_type == 'Vimeo Video') {
-        ?>data-jarallax-video="<?php echo esc_url($video_url); ?>"<?php
-    } ?>>
 	<div class="single_tour_header_content">
 		<div class="standard_wrapper">
 			<?php
-                //Get tour gallery
-                $tour_gallery = get_post_meta($current_page_id, 'tour_gallery', true);
-
-    if (!empty($tour_gallery)) {
-        //Get gallery images
-        $all_photo_arr = get_post_meta($tour_gallery, 'wpsimplegallery_gallery', true);
-
-        $image_url = '';
-        if (isset($all_photo_arr[0]) && !empty($all_photo_arr[0])) {
-            $image_url = wp_get_attachment_image_src($all_photo_arr[0], 'original', true);
-        } ?>
-			<a href="<?php echo esc_url($image_url[0]); ?>" id="single_tour_gallery_open" class="button fancy-gallery"><span class="ti-camera"></span><?php esc_html_e('View Photos', 'grandtour'); ?></a>
-			<div style="display:none;">
-				<?php
-                if (!empty($all_photo_arr)) {
-                    foreach ($all_photo_arr as $key => $photo_id) {
-                        if ($key > 0) {
-                            $image_url = '';
-
-                            if (!empty($photo_id)) {
-                                $image_url = wp_get_attachment_image_src($photo_id, 'original', true);
-                            }
-
-                            //Get image meta data
-                            $image_caption = get_post_field('post_excerpt', $photo_id); ?>
-				 <a id="single_tour_gallery_image<?php echo esc_attr($key); ?>" href="<?php echo esc_url($image_url[0]); ?>" title="<?php echo esc_attr($image_caption); ?>" class="fancy-gallery"></a>
-				<?php
-                        }
-                    }
-                } ?>
-			</div>
-			<?php
-    } ?>
-			
-			<?php
-                //Get tour video review
-                $tour_video_preview = get_post_meta($current_page_id, 'tour_video_preview', true);
-
-    if (!empty($tour_video_preview)) {
-        ?>
-			<a href="#video_review<?php echo esc_attr($current_page_id); ?>" id="single_tour_video_preview_open" class="button" data-type="inline"><span class="ti-control-play"></span><?php esc_html_e('Video Preview', 'grandtour'); ?></a>
-			
-			<div id="video_review<?php echo esc_attr($current_page_id); ?>" class="tour_video_preview_wrapper" style="display:none;"><?php echo $tour_video_preview; ?></div>
-			<?php
-    } ?>
-			
-			<?php
-                //Get tour price
-                $tour_price = get_post_meta($current_page_id, 'tour_price', true);
 
     if (!empty($product['prices'])) {
-        $tour_discount_price = get_post_meta($current_page_id, 'tour_discount_price', true); ?>
+        ?>
 			<div class="single_tour_header_price">
 				<div class="single_tour_price">
 					<?php
@@ -169,3 +99,57 @@ if (isset($product['media'])) {
     } ?><?php if (!empty($grandtour_page_content_class)) {
         echo esc_attr($grandtour_page_content_class);
     } ?>">
+
+
+<script>
+
+var slideIndex = 1;
+showSlides(slideIndex);  
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  var navs = document.getElementsByClassName("nav");
+  if (slides.length == 1) { 
+      for (i = 0; i < navs.length; i++) {
+      navs[i].className = nav[i].style.display = "none";  
+  }
+    } 
+  if (n > slides.length) { slideIndex = 1;} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";   
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+// var slideIndex = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none"; 
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1} 
+//   slides[slideIndex-1].style.display = "block"; 
+//   setTimeout(showSlides, 5000); // Change image every 5 seconds
+// }
+</script>
